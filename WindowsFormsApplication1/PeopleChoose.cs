57aa5f7294b296ef7a61b9de9b6d33c0eb377d7a
King-1997 <1597595060@qq.com>
2019-04-12 10:21:11 +0800

    将人员按照入职时间查询的逻辑优化，设置开始时间都为该月1号，而不需要每个条件都去赋值。

--- WindowsFormsApplication1/PeopleChoose.cs
+++ WindowsFormsApplication1/PeopleChoose.cs
@@ -115,31 +115,33 @@ namespace WindowsFormsApplication1
 
                 int month = 0;
                 int.TryParse(cBx_workerEntryMonth.SelectedItem.ToString(), out month);
+                entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");//设置开始时间都为该月1号
                 if (month == 02)//当月份为2时判断是否为闰年
                 {
                     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
                     {
 
-                        entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
+                        //entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
                         entryTime2 = Convert.ToDateTime(year + "-" + month + "-29");
                     }
                     else
                     {
-                        entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
+                        //entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
                         entryTime2 = Convert.ToDateTime(year + "-" + month + "-28");
                     }
                 }
                 //当月份不为2时，判断当前月份是否有31号
                 if (month == 01 || month == 03 || month == 05 || month == 07 || month == 08 || month == 10 || month == 12)
                 {
-                    entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
+                    //entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
                     entryTime2 = Convert.ToDateTime(year + "-" + month + "-31");
                 }
                 else
                 {
-                    entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
+                    //entryTime1 = Convert.ToDateTime(year + "-" + month + "-01");
                     entryTime2 = Convert.ToDateTime(year + "-" + month + "-30");
                 }
+
                 sql += "select u.u_name,g.g_group,u.u_sex,u_phone,u_entryTime from [User] u,[group] g where u.g_id = g.g_id and u.u_entryTime >= '" + entryTime1 + "' and u.u_entryTime <= '" + entryTime2 + "'";
             }
                 Console.WriteLine("查询语句：" + sql);
